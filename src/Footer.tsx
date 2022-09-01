e6e421ad88cd9b7c27d71ac185d9efa4220081cd
tenpaMk2 <wavyroom@gmail.com>
2022-09-01 16:33:22 +0900

    :zap: add github link to footer.

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="flex justify-center text-sm italic text-gray-400">
+    <footer className="flex flex-col items-center text-sm italic text-gray-400">
       <p>
         <a href="https://twitter.com/tenpaMk2" className="underline">
           Author: tenpaMk2
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://github.com/tenpaMk2/react-diagram-maker"
+          className="underline"
+        >
+          Source code: github
         </a>
       </p>
     </footer>
