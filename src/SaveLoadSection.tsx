39bec61ceab5cfdaa20fe35857a6c8dd211089af
tenpaMk2 <wavyroom@gmail.com>
2022-09-06 15:47:46 +0900

    :lipstick: change save button text.

--- src/SaveLoadSection.tsx
+++ src/SaveLoadSection.tsx
@@ -67,7 +67,7 @@ const SaveLoadSection = ({ state, dispatch }: Props) => {
             onClick={download}
             className="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
-            保存
+            Download JSON
           </button>
         </section>
       </section>
