9b0a08687668c15d89dbbd9db37614f84b5bf5a7
tenpaMk2 <wavyroom@gmail.com>
2023-05-09 23:41:43 +0900

    :recycle: Refactor.

--- src/Station.tsx
+++ src/Station.tsx
@@ -7,23 +7,21 @@ type Props = {
   removeStation: (staionName: string) => void;
 };
 
-const Station = ({ stationName, isLast, removeStation }: Props) => {
-  return (
-    <div className="flex gap-4">
-      <div className="flex flex-col items-center">
-        <div>
-          <div className="flex h-10 w-10 items-center justify-center rounded-full border">
-            <DownArrow />
-          </div>
+const Station = ({ stationName, isLast, removeStation }: Props) => (
+  <div className="flex gap-4">
+    <div className="flex flex-col items-center">
+      <div>
+        <div className="flex h-10 w-10 items-center justify-center rounded-full border">
+          <DownArrow />
         </div>
-        <div className="h-full w-px bg-gray-300"></div>
       </div>
-      <div className={`${isLast ? `pt-1` : `pt-2 pb-8`}`}>
-        <p className="text-xl font-bold text-gray-600">{stationName}</p>
-      </div>
-      <TrashButton onClick={() => removeStation(stationName)} />
+      <div className="h-full w-px bg-gray-300"></div>
     </div>
-  );
-};
+    <div className={isLast ? `pt-1` : `pb-8 pt-2`}>
+      <p className="text-xl font-bold text-gray-600">{stationName}</p>
+    </div>
+    <TrashButton onClick={() => removeStation(stationName)} />
+  </div>
+);
 
 export default Station;
